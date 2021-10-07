d277ad481eee166cb2c119c7cfa71e99a3b6bc37
radhikaGupta03 <91174937+radhikaGupta03@users.noreply.github.com>
2021-10-07 10:04:01 +0530

    Create Task-03.cpp

---
+++ Practical-06/Task-03.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    string str;
+    cout << "Enter the string: ";
+    cin >> str;
+    reverse(std.begin(), str.end());
+    cout << "Reverse string is: " << str << endl;
+    return 0;
+}
