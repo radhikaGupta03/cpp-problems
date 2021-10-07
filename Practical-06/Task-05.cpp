c7a9174b59f886ed301f4713379defde5b0eba35
radhikaGupta03 <91174937+radhikaGupta03@users.noreply.github.com>
2021-10-07 10:11:14 +0530

    Create Task-05.cpp

---
+++ Practical-06/Task-05.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string s1 = "abc";
+    string s2 = "ABC";
+    int r;
+    r = s1.compare(s2);
+    cout << r << endl;
+    return 0;
+}
